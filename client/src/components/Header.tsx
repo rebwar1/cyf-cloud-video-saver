69ca54c761550e1bce6fec79a33709b5073dff65
rebwar <rebwar1.azizi@gmail.com>
2024-02-05 08:45:17 +0000

    Add title Deploy with GitHub Action

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,14 +1,25 @@
-import React from 'react';
-import youtubeLogo from './images/youtube-logo.png';
+import React from "react";
+import youtubeLogo from "./images/youtube-logo.png";
 
 function Header(): JSX.Element {
   return (
-    <header className="flex items-center justify-center pb-8 pt-8 text-3xl sm:px-8 sm:pt-8 sm:text-4xl sm:font-bold sm:tracking-wider lg:text-6xl">
+    <header className="flex items-center justify-between pb-8 pt-8 text-3xl sm:px-8 sm:pt-8 sm:text-4xl sm:font-bold sm:tracking-wider lg:text-6xl">
       <img
         src={youtubeLogo}
         alt="YouTube Logo"
         className="w-13 h-9 sm:h-16 sm:w-24"
       />
+      <div className="popover-container">
+        <button
+          type="button"
+          className="btn btn-secondary"
+          data-bs-toggle="popover"
+          data-bs-placement="left"
+          data-bs-content="Deploy with GitHub Action"
+        >
+          ...
+        </button>
+      </div>
     </header>
   );
 }
